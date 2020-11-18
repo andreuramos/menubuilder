4f19851de2f796d1b13d4976b07460d51ec9b84c
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2020-11-18 10:30:07 +0100

    rename test

--- src/menubuilder/services/MenuFactory.spec.ts
+++ src/menubuilder/services/MenuFactory.spec.ts
@@ -2,7 +2,7 @@ import {Menu} from "../entities/Menu";
 import {MenuFactory} from "./MenuFactory";
 
 describe("Menu Factory", () => {
-    it ("does something", () => {
+    it ("Returns a menu", () => {
         const factory = new MenuFactory();
 
         const menu = factory.execute();
