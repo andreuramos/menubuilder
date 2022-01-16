b26e6a374dc4396d2fa03b1c1ca8b1ac04c6d887
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-01-16 12:34:22 +0100

    fix again migration to have camelCase columns

--- src/database/migrations/1614716340684-CreateMenuTable.ts
+++ src/database/migrations/1614716340684-CreateMenuTable.ts
@@ -5,15 +5,15 @@ export class CreateMenuTable1614716340684 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query("CREATE TABLE IF NOT EXISTS menus (" +
             "id SERIAL PRIMARY KEY," +
-            "week_number INT," +
-            "created_at TIMESTAMP," +
-            "updated_at TIMESTAMP" +
+            "weekNumber INT," +
+            "createdAt TIMESTAMP," +
+            "updatedAt TIMESTAMP" +
             ");");
 
         await queryRunner.query("CREATE TABLE IF NOT EXISTS dishes_menus (" +
-            "menu_id INT," +
-            "dish_id INT," +
-            "menu_slot VARCHAR(50)" +
+            "menuId INT," +
+            "dishId INT," +
+            "menuSlot VARCHAR(50)" +
             ");");
     }
 
