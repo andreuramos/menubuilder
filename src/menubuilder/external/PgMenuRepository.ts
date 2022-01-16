500493d529936e412f547bdc2531c6b105f48d07
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2022-01-16 12:42:14 +0100

    save method for menu in repository

--- src/menubuilder/external/PgMenuRepository.ts
+++ src/menubuilder/external/PgMenuRepository.ts
@@ -1,15 +1,49 @@
 import {getConnection} from "typeorm";
 import {Menu} from "../entities/Menu";
 import {IMenuRepository} from "../interfaces/IMenuRepository";
+import {WeekCalculator} from "../services/WeekCalculator";
+import {DishesMenus} from "./entities/DishesMenus";
+import {Menus} from "./entities/Menus";
 
 export class PgMenuRepository implements IMenuRepository
 {
-    public save(menu: Menu): void {
+    private weekCalculator;
 
-        const today_week = new Date().format('"')
+    public constructor() {
+        this.weekCalculator = new WeekCalculator();
+    }
+    public async save(menu: Menu) {
+        const timestamp = new Date();
+        const dbMenu = {
+            weeknumber: menu.getWeekNumber(),
+            createdat: timestamp,
+            updatedat: timestamp,
+        };
+        const insertResult = await getConnection()
+            .getRepository(Menus)
+            .insert(dbMenu);
 
-        getConnection().getRepository(Menus)
-            .insert()
+        await this.linkDishes(insertResult.identifiers[0].id, menu.dishes());
     }
 
+    private ormToEntity(dbMenu: Menus): Menu
+    {
+        const menu = new Menu();
+        menu.setWeekNumber(this.weekCalculator.execute(new Date(dbMenu.createdat)));
+
+        return menu;
+    }
+
+    private async linkDishes(menuId: number, dishes: object)
+    {
+        for (const slot of Object.keys(dishes)) {
+            const dishMenu = {
+                menuid: menuId,
+                dishid: dishes[slot].id,
+                menuslot: slot,
+            };
+            await getConnection().getRepository(DishesMenus)
+                .insert(dishMenu);
+        }
+    }
 }
